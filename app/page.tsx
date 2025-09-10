ad58b6109f1c69ec3a61dbb16b76664bfad0567c
gj1214 <vearochea8@gmail.com>
2025-09-10 20:59:46 +0700

    Updated homepage version00

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,8 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I am an AI and Machine Learning engineer with a strong focus on developing theorical practical, 
+        data-driven solutions and hand-on projects in my Profolio and Work Experiences`}
       </p>
       <div className="my-8">
         <BlogPosts />
