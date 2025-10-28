145f7aeb149151052c711e233acdd7b2e8d6cf27
gj1214 <vearochea8@gmail.com>
2025-10-29 00:44:56 +0700

    Site map got updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,14 @@ import { getBlogPosts } from 'app/blog/utils'
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
+  // Map blog posts
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  // Add static routes: Home, Blog, Contact
+  let routes = ['', '/blog', '/contact'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
